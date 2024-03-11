e31a4a99abdaa1e988e6ea368493c46c1c53bcdb
dataNerd65 <pkiarie811@gmail.com>
2024-03-11 08:29:47 +0300

    Still improving and added a readme file

--- comp.py
+++ comp.py
@@ -11,8 +11,6 @@ user = config['mysql']['user']
 password = config['mysql']['password']
 database = config['mysql']['database']
 
-
-
 try:
     mydb = mysql.connector.connect(
         host=host,
@@ -35,41 +33,58 @@ except ValueError as e:
     print("An error occurred: ", str(e))
 
 
-def look_item():
-    global user_choice  # Access the global user_choice variable
+def look_item(user_choice):
     print("Let me check if the item(s) is present.")
     lemmatizer = WordNetLemmatizer()
     lemmatized_choice = lemmatizer.lemmatize(user_choice)
-    print(lemmatized_choice)
-    
-look_item()
+    #print("Lemmatized choice:", lemmatized_choice)
+    return lemmatized_choice
 
-class Electronics:
-    def __init__(self, name, model, generation):
-        self.name = name
-        self.model = model
-        self.generation = generation
+def check_item_in_Database(user_choice):
+    try:
+        with mydb.cursor() as mycursor:
+            mycursor.execute("SELECT * FROM itemsinfo WHERE item_type = %s", (user_choice,))
 
-    def get_name(self):
-        return self.name
-    
-    def get_model(self):
-        return self.model
-    
-    def get_generation(self):
-        return self.generation
-    
-    def check_item_in_Database(self, item_name):
-        db = mydb
-        mycursor = db.cursor()
-        mycursor.execute("SELECT * FROM items WHERE name = %s", (item_name,))
-        myresult = mycursor.fetchall()
+            myresult = mycursor.fetchall()
         return len(myresult) > 0
+    except mysql.connector.Error as err:
+        print("An error occurred while querying the database:", err)
+        return False
 
-class Computer(Electronics):
-    def __init__(self, name, model, generation):
-        super().__init__(name, model, generation)
+lemmatized_choice = look_item(user_choice)
+is_in_database = check_item_in_Database(lemmatized_choice)
 
-class Phone(Electronics):
-    def __init__(self, name, model, generation):
-        super().__init__(name, model, generation)
+#print("Is the item in the database?", is_in_database)
+
+if is_in_database is True:
+    print(f"Yes we have {user_choice}.")
+else:
+    print("Sorry, we do not have what you want.")
+    
+
+#print(f"We have the following types of {user_choice}.")
+
+def get_item_details(user_choice):
+    try:
+        with mydb.cursor() as mycursor:
+            mycursor.execute("SELECT * FROM itemsinfo WHERE item_type = %s", [user_choice])
+            item_details = mycursor.fetchall()
+        return item_details
+    except mysql.connector.Error as err:
+        print("An error occurred while querying the database:", err)
+        return None
+
+# Fetch item details from the database
+item_details = get_item_details(lemmatized_choice)
+
+if item_details:
+    print(f"Here are the details of {user_choice}:")
+    print("ID\tBrand\tModel\tQuantity\tPrice\tDescription\tCreated At")
+    for item in item_details:
+        print(f"{item[0]}\t{item[2]}\t{item[3]}\t{item[4]}\t{item[5]}\t{item[6]}\t{item[7]}")
+else:
+    print("Sorry, we do not have details for the requested item.")
+
+
+
+
